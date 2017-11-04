59d2402be2d23b858e3e6e2a817c73b35a211d55
sseen <ssnfee@gmail.com>
2017-11-04 13:51:19 +0800

    first done

--- CADisplaylinkDemo/ViewController.swift
+++ CADisplaylinkDemo/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblName: UILabel!
+    var displaylink:CADisplayLink!
+    var content = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblName.text = "\(content)"
+        self.displaylink = CADisplayLink(target: self, selector: #selector(updateText) )
+        //displaylink.preferredFramesPerSecond = 1
+        displaylink.frameInterval = 1
+        //displaylink.isPaused = true
+        displaylink.add(to: RunLoop.current, forMode: .commonModes)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func updateText() {
+        content += 1
+        lblName.text = "\(content)"
+    }
+    
+    func startAnimation() {
+        displaylink.isPaused = false
+    }
+    
+    func stopAnimation() {
+        displaylink.isPaused = true
+        displaylink.invalidate()
+    }
 
+    @IBAction func ck(_ sender: Any) {
+        self.startAnimation()
+    }
 }
 
